1d37146eb8a0e54c20adc5f7982bd2a5e0adf655
Nikita Rudenko <rudenkornk@gmail.com>
2022-05-19 21:16:55 +0300

    Add k_max and reverse_list problems

---
+++ reverse_list/solution.hpp
@@ -0,0 +1,23 @@
+#include <cassert>
+#include <climits>
+#include <numeric>
+#include <vector>
+
+template <typename T> struct list_node final {
+  T data_;
+  list_node *next_;
+  list_node(T data,
+            list_node *next) noexcept(std::is_nothrow_move_constructible_v<T>)
+      : data_(std::move(data)), next_(next) {}
+};
+
+template <typename T> list_node<T> *reverse_list(list_node<T> *head) noexcept {
+  list_node<T> *new_head = nullptr;
+  while (head) {
+    list_node<T> *next = head->next_;
+    head->next_ = new_head;
+    new_head = head;
+    head = next;
+  }
+  return new_head;
+}
---
+++ reverse_list/unit_tests/test_1.cpp
@@ -0,0 +1,40 @@
+#define BOOST_TEST_MODULE Test
+
+#include <cassert>
+#include <forward_list>
+#include <utility>
+#include <vector>
+
+#include <boost/test/included/unit_test.hpp>
+
+#include "solution.hpp"
+
+void test_reverse_list(std::initializer_list<int> init) {
+  // Use vector as container to avoid memory leaks
+  std::vector<list_node<int>> container;
+  size_t n = init.size();
+  container.reserve(n);
+
+  list_node<int> *head = nullptr;
+  for (auto it = std::rbegin(init); it != std::rend(init); ++it) {
+    container.emplace_back(*it, head);
+    head = &container.back();
+  }
+  std::forward_list<int> reference_list(init);
+  reference_list.reverse();
+  head = reverse_list(head);
+
+  auto *it = head;
+  auto reference_it = reference_list.begin();
+  while (n-- != 0) {
+    assert(it->data_ == *reference_it);
+    it = it->next_;
+    ++reference_it;
+  }
+}
+
+BOOST_AUTO_TEST_CASE(main_test) {
+  test_reverse_list({});
+  test_reverse_list({0});
+  test_reverse_list({0, 1, 2, 3, 4});
+}
