51dadd32b37f4ec6936a19fba71b4a42fa19818d
Nikita Rudenko <rudenkornk@gmail.com>
2022-06-12 07:21:18 +0300

    Refactor size_t to std::size_t

--- k_max/solution.hpp
+++ k_max/solution.hpp
@@ -3,16 +3,16 @@
 #include <set>
 #include <vector>
 
-std::vector<int> inline k_max(std::vector<int> const &nums, size_t k) {
+std::vector<int> inline k_max(std::vector<int> const &nums, std::size_t k) {
   // O(n) memory, O(n * log(k)) time
   std::vector<int> result{};
   result.reserve(nums.size() + 1 - k);
   std::multiset<int> window{};
-  for (size_t i = 0; i != k; ++i)
+  for (std::size_t i = 0; i != k; ++i)
     window.insert(nums[i]);
 
   result.push_back(*window.rbegin());
-  for (size_t i = 1; i != nums.size() + 1 - k; ++i) {
+  for (std::size_t i = 1; i != nums.size() + 1 - k; ++i) {
     window.erase(window.find(nums[i - 1]));
     window.insert(nums[k + i - 1]);
     result.push_back(*window.rbegin());
