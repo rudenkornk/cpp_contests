5939a423abad294157d3ca33fd3ffafbffe710ef
Nikita Rudenko <rudenkornk@gmail.com>
2022-06-13 01:58:47 +0300

    Convert k_max to physical lib instead of interface

--- k_max/include/k_max/solution.hpp
+++ k_max/include/k_max/solution.hpp
@@ -1,23 +1,5 @@
 #pragma once
 
-#include <cassert>
-#include <numeric>
-#include <set>
 #include <vector>
 
-std::vector<int> inline k_max(std::vector<int> const &nums, std::size_t k) {
-  // O(n) memory, O(n * log(k)) time
-  std::vector<int> result{};
-  result.reserve(nums.size() + 1 - k);
-  std::multiset<int> window{};
-  for (std::size_t i = 0; i != k; ++i)
-    window.insert(nums[i]);
-
-  result.push_back(*window.rbegin());
-  for (std::size_t i = 1; i != nums.size() + 1 - k; ++i) {
-    window.erase(window.find(nums[i - 1]));
-    window.insert(nums[k + i - 1]);
-    result.push_back(*window.rbegin());
-  }
-  return result;
-}
+std::vector<int> k_max(std::vector<int> const &nums, std::size_t k);
---
+++ k_max/solution.cpp
@@ -0,0 +1,23 @@
+#include <cassert>
+#include <numeric>
+#include <set>
+#include <vector>
+
+#include "k_max/solution.hpp"
+
+std::vector<int> k_max(std::vector<int> const &nums, std::size_t k) {
+  // O(n) memory, O(n * log(k)) time
+  std::vector<int> result{};
+  result.reserve(nums.size() + 1 - k);
+  std::multiset<int> window{};
+  for (std::size_t i = 0; i != k; ++i)
+    window.insert(nums[i]);
+
+  result.push_back(*window.rbegin());
+  for (std::size_t i = 1; i != nums.size() + 1 - k; ++i) {
+    window.erase(window.find(nums[i - 1]));
+    window.insert(nums[k + i - 1]);
+    result.push_back(*window.rbegin());
+  }
+  return result;
+}
