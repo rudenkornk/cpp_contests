5006559e91adad27ea714f24671d3b98d9c21752
Nikita Rudenko <rudenkornk@gmail.com>
2022-07-30 18:10:23 +0300

    Amend conan targets
    Now, they do not throw if my crazy conan configs are not found,
    but rather just pick the default one.

--- scripts/pick_conan_profile.py
+++ scripts/pick_conan_profile.py
@@ -12,22 +12,28 @@ def eprint(*args, **kwargs):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Find appropriate conan profile matching query and generate conan options for it.",
+        description="Find appropriate conan profile matching query.",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("query", type=str, help="regex query for desired profile")
     args = parser.parse_args(sys.argv[1:])
-    re_compiled = re.compile(args.query)
+    query = args.query
+    query_compiled = re.compile(query)
+    default_profile = "default"
 
     profiles = os.popen("conan profile list").readlines()
     profiles = [profile.strip() for profile in profiles]
-    filtered_profiles = [profile for profile in profiles if re_compiled.search(profile)]
+    filtered_profiles = [p for p in profiles if query_compiled.search(p)]
     if len(filtered_profiles) == 0:
-        eprint("WARNING: No matching conan profiles found for query '{}'.".format(args.query))
+        eprint("WARNING: No matching conan profiles found for query '{}'.".format(query))
         eprint("Will try to use default profile.")
-        print("")
-        exit(0)
+        if default_profile not in profiles:
+            eprint("ERROR: Default profile '{}' not found.".format(default_profile))
+            eprint("Did you forget to run 'conan config init'?")
+            sys.exit(1)
+        print(default_profile)
+        sys.exit(0)
     if len(filtered_profiles) > 1:
-        eprint("WARNING: Found more than one matching profile for query '{}':".format(args.query))
+        eprint("WARNING: Found more than one matching profile for query '{}':".format(query))
         eprint("\n".join(filtered_profiles))
         eprint("Will be picked the first one.")
-    print("--profile:host {0} --profile:build {0}".format(filtered_profiles[0]))
+    print(filtered_profiles[0])
