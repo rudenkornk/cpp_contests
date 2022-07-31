0b831d69c5d39dcede26af9d24e0755c0069b370
Nikita Rudenko <rudenkornk@gmail.com>
2022-07-31 16:39:07 +0300

    Adjust variable names in cache problems

--- perfect_cache/include/perfect_cache/solution.hpp
+++ perfect_cache/include/perfect_cache/solution.hpp
@@ -6,6 +6,6 @@ namespace cpp_contests {
 
 std::size_t perfect_cache(std::vector<int> const &keys,
                           std::size_t max_size_in_bytes,
-                          std::size_t value_size);
+                          std::size_t value_size_in_bytes);
 
 } // namespace cpp_contests
