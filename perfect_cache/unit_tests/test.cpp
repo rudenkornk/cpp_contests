1566513583d4f8297bab9630e325934deb34eada
Nikita Rudenko <rudenkornk@gmail.com>
2022-07-31 08:19:18 +0300

    Move cache problems benchmarks to perfect_cache dir

--- perfect_cache/unit_tests/test.cpp
+++ perfect_cache/unit_tests/test.cpp
@@ -19,6 +19,28 @@
 
 using namespace cpp_contests;
 
+// Assume that sizeof(Value) >> sizeof(Key), then
+// how many cache hits would be if you can store at max 'pages' number of Values
+static std::size_t perfect_cache_wrapper(std::vector<int> const &keys,
+                                         std::size_t pages) {
+  return perfect_cache(keys, pages * sizeof(int), 0);
+}
+
+BOOST_AUTO_TEST_CASE(correctness_test) {
+  BOOST_TEST(perfect_cache_wrapper({}, 0) == std::size_t{0});
+  BOOST_TEST(perfect_cache_wrapper({1}, 0) == std::size_t{0});
+  BOOST_TEST(perfect_cache_wrapper({1, 2, 3, 4}, 0) == std::size_t{0});
+  BOOST_TEST(perfect_cache_wrapper({1, 2, 3, 4}, 1) == std::size_t{0});
+  BOOST_TEST(perfect_cache_wrapper({1, 1, 1, 1}, 1) == std::size_t{3});
+  BOOST_TEST(perfect_cache_wrapper({1, 2, 1, 2}, 1) == std::size_t{1});
+  BOOST_TEST(perfect_cache_wrapper({1, 2, 2, 1}, 1) == std::size_t{1});
+  BOOST_TEST(perfect_cache_wrapper({1, 2, 3, 4, 5, 6, 1}, 1) == std::size_t{1});
+  BOOST_TEST(perfect_cache_wrapper({1, 2, 3, 4, 5, 6, 1}, 2) == std::size_t{1});
+  BOOST_TEST(perfect_cache_wrapper({1, 2, 3, 4, 5, 2, 1}, 2) == std::size_t{2});
+  BOOST_TEST(perfect_cache_wrapper({1, 2, 3, 1, 2, 4, 5, 1, 7, 3, 2, 6, 1, 2},
+                                   4) == std::size_t{7});
+}
+
 using Key = int;
 static constexpr std::size_t typical_web_page_size_in_bytes =
     std::size_t{2} * 1024 * 1024;
