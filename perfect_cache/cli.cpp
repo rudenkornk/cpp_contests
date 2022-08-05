07e5218ce9ef6f00d9eb5671281fd7f2b76a3bb1
Nikita Rudenko <rudenkornk@gmail.com>
2022-08-05 13:29:05 +0300

    Handle potential errors from cin in cache problems

--- perfect_cache/cli.cpp
+++ perfect_cache/cli.cpp
@@ -9,17 +9,26 @@
 int main() {
   std::size_t cache_length = 0;
   std::size_t n_elements = 0;
-  std::cin >> cache_length;
-  std::cin >> n_elements;
   std::vector<int> elements{};
-  elements.reserve(n_elements);
-  for (std::size_t i = 0; i < n_elements; ++i) {
-    int n = 0;
-    std::cin >> n;
-    elements.push_back(n);
+
+  std::cin.exceptions(std::ios_base::failbit);
+
+  try {
+    std::cin >> cache_length;
+    std::cin >> n_elements;
+    elements.reserve(n_elements);
+    for (std::size_t i = 0; i < n_elements; ++i) {
+      int n = 0;
+      std::cin >> n;
+      elements.push_back(n);
+    }
+  } catch (std::ios_base::failure const &e) {
+    std::cout << "Incorrect input! \n";
+    std::cout << e.what() << "\n";
   }
 
-  auto lru = cpp_contests::perfect_cache(elements, cache_length * sizeof(int), 0);
+  auto lru =
+      cpp_contests::perfect_cache(elements, cache_length * sizeof(int), 0);
   std::cout << lru << "\n";
   return 0;
 }
